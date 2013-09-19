a8eb2cf1fe927e9c92adc9e76b515572e3418558
A-Sandwich <kbburkholder@gmail.com>
2013-09-19 13:10:54 -0400

    Changed file structure to be more organized. File references in login pages will have to be fixed.

---
+++ assets/files/db_connect.php
@@ -0,0 +1,7 @@
+<?php
+define("HOST", "mysql4.000webhost.com"); // The host you want to connect to.
+define("USER", "a5615996_garage"); // The database username.
+define("PASSWORD", "a5615996_novus"); // The database password. 
+define("DATABASE", "dkn!novus"); // The database name.
+
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
