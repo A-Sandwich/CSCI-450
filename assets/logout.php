90f0f6622cf767826bf071d4d140510cb62e9dae
thedouglenz <thedouglenz@gmail.com>
2013-09-27 15:39:59 -0400

    Logging out should now work as desired
    As desired........

--- assets/logout.php
+++ assets/logout.php
@@ -7,5 +7,5 @@ if(isset($_GET['logout'])) {
 	setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 	session_destroy();
 	echo $_SESSION['username'];
-	//header('Location: ../index.php');
+	header('Location: ../index.php');
 }
