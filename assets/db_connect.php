e4ebe8bcad7195060daee1734fbb8ffa00de6388
thedouglenz <thedouglenz@gmail.com>
2013-11-03 15:10:18 -0500

    Updated db_connect
    new database constants defined for the new host

--- assets/db_connect.php
+++ assets/db_connect.php
@@ -1,7 +1,18 @@
 <?php
-define("HOST", "mysql4.000webhost.com"); // The host you want to connect to.
+/* Old host db connect info
+ * 
+ * define("HOST", "mysql4.000webhost.com"); // The host you want to connect to.
 define("USER", "a5615996_novus"); // The database username.
 define("PASSWORD", "dkn!novus"); // The database password. 
 define("DATABASE", "a5615996_garage"); // The database name.
 
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);*/
+
+
+define("HOST", "localhost"); // The host you want to connect to.
+define("USER", "novusgar_us"); // The database username.
+define("PASSWORD", "wedontsuck"); // The database password. 
+define("DATABASE", "novusgar_database"); // The database name.
+
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
--- assets/files/db_connect.php
+++ assets/files/db_connect.php
@@ -1,7 +1,18 @@
 <?php
+/* Old host db connect info
+ * 
+ * define("HOST", "mysql4.000webhost.com"); // The host you want to connect to.
+define("USER", "a5615996_novus"); // The database username.
+define("PASSWORD", "dkn!novus"); // The database password. 
+define("DATABASE", "a5615996_garage"); // The database name.
+
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);*/
+
+
 define("HOST", "localhost"); // The host you want to connect to.
-define("USER", "novusgar_db"); // The database username.
-define("PASSWORD", "wedontsuck=false"); // The database password. 
+define("USER", "novusgar_us"); // The database username.
+define("PASSWORD", "wedontsuck"); // The database password. 
 define("DATABASE", "novusgar_database"); // The database name.
 
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE)or die('Could not connect: ' . mysql_error());
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
