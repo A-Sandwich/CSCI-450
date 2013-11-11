aa160868f8425910333e5a3c9d9108f49e637430
A-Sandwich <kbburkholder@gmail.com>
2013-11-10 21:00:37 -0500

    index.php is now db driven.
    You can now update the homepage by simply changing the contents of the
    carousel table.

--- assets/files/carousel.php
+++ assets/files/carousel.php
@@ -1,78 +1,72 @@
 <?php
-/*
-	$con = mysql_connect("localhost", "novusgar_us", "wedontsuck", "novusgar_database");
+	$con = mysqli_connect("localhost", "novusgar_us", "wedontsuck", "novusgar_database");
 	if (! $con){
-    echo'connection failed'; 
-} 
+	    echo'connection failed'; 
+	}else{
+		//echo''.$con.'';
+	};
 	$query = sprintf('SELECT * FROM carousel');
-	$rows = mysql_query($sql);
+	$qryResult = mysqli_query($con, $query);
 	
-	if (! $rows){ 
-    // probably a syntax error in your SQL, 
-    // but could be some other error
-    //throw new Db_Query_Exception("DB Error: " . mysql_error());
-    echo'failed'; 
-}
-	mysql_close($con);
-	//echo''.mysql_num_rows($rows).'';*/
-
+	if (! $qryResult){ 
+	    echo'failed'; 
+	}
+	mysqli_close($con);	
+		
+	echo '
+		<!-- Carousel
+		================================================== -->
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">';
 	
-	echo'..';
-		echo '
-			<!-- Carousel
-			================================================== -->
-			<div id="myCarousel" class="carousel slide" data-ride="carousel">
-			  <!-- Indicators -->
-			  <div class="carousel-inner">
-		  ';
-		$counter = 1;
-		$active = 'active';
-		echo'<ol class="carousel-indicators">';
-		if ($handle = opendir(__DIR__.'/../images/background_large')) {
-			echo'tiffany was right again...';
-			 while (false !== ($entry = readdir($handle))) {
-				 if (strpos($entry,'png') !== false) {
-					 echo'<li data-target="#myCarousel" data-slide-to="'.$counter.'" class="'.$active.'"></li>';
-					$active = '';
-					$counter = $counter+1;
-				};
-			 };
-			
-		};
-		echo'</ol>';
-		$active = 'active';
-		closedir($handle);
-		if ($handle = opendir(__DIR__.'/../images/background_large')) {
-			$counter = 1;
-			echo'<style>';
-			while (false !== ($entry = readdir($handle))) {
-				if (strpos($entry,'png') !== false && $entry != '') {
-					echo'
-						.horse'.$counter.'{
-							background-image: url("assets/images/background_large/'.$entry.'");	  
-							background-size: cover;        
-					   }';
-					
-					$counter = $counter + 1;
-				};
-			};
-			echo'</style>';
-			closedir($handle);
-			for($i=1;$i<$counter;$i++){
-				echo'
-				<div class="item '.$active.' horse'.$i.'">
-				  <div id="horse'.$i.'" class="container">
-					<div class="carousel-caption">
-					  <h1>Novus</h1>
-					  <p>Novus Garage is not a one stop shop, in fact it is not evena shop.</p>
-					</div>
-				  </div>
-				</div>'	;
-				$active = '';
-			};
-													  echo'</div><a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-			  <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-			</div><!-- /.carousel -->';
-		};
+	$counter = 0;
+	$active = 'active';
+	echo'	<!-- Indicators --><ol class="carousel-indicators">';
+	while($row = mysqli_fetch_assoc($qryResult)){
+		echo'<li data-target="#myCarousel" data-slide-to="'.$counter.'" class="'.$active.'"></li>';
+		$counter++;
+		$active='';
+	}//end while
+	echo'</ol>';
+	mysqli_data_seek($qryResult, 0);
+	echo'
+		
+			<div class="carousel-inner">
+	';
+	//<!-- Create indicatiors/dots for carousel -->
 	
+	
+	//Create styles + carousel items.
+	$counter = 0;
+	$active = 'active';
+	while($row = mysqli_fetch_assoc($qryResult)){   
+	   echo'
+		<div class="item '.$active.' horse'.$counter.'">
+		  <div id="horse'.$counter.'" class="container">
+			<div class="carousel-caption">
+			  <h1>Novus</h1>
+			  <p>'.$row['Content'].'</p>
+			</div>
+		  </div>
+		</div>'	;
+		$counter++;
+		$active = '';
+	}
+	
+	echo'
+		</div><a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+		</div><!-- /.carousel -->
+	';
+	mysqli_data_seek($qryResult, 0);
+	$counter = 0;
+	  while($row = mysqli_fetch_assoc($qryResult)){
+			echo'
+			<style>
+			.horse'.$counter.'{
+				background-image: url("assets/images/background_large/'.$row['Img'].'");	  
+				background-size: cover;        
+		   }
+		   </style>';
+		   $counter++;
+	  };	
 ?>
