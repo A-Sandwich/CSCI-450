8451247d369c1a70ef71c8826aed80c68d99818c
thedouglenz <thedouglenz@gmail.com>
2013-11-17 16:19:43 -0500

    users_cars table now linked to cars table via...
    users_cars.car_id <--> cars.ID

--- assets/process_car.php
+++ assets/process_car.php
@@ -9,12 +9,13 @@ if(isset($_POST['carName'], $_POST['mileage'], $_POST['carMake'], $_POST['carMod
 	$vin = $_POST['vin']; $engineType = $_POST['engineType'];
 		
 	//Check if the car entered by the user is in the extensive car table
-	$check_car_exists_query = $mysqli->prepare('SELECT ID FROM cars WHERE Make = ? AND Model = ? AND Year = ? LIMIT 1');
-	$check_car_exists_query->bind_param('ssi', $carMake, $carModel, $carYear);
+	$check_car_exists_query = $mysqli->prepare('SELECT ID FROM cars WHERE Make = ? AND Model = ? AND Year = ? AND Engine = ? LIMIT 1');
+	$check_car_exists_query->bind_param('ssis', $carMake, $carModel, $carYear, $engineType);
 	$check_car_exists_query->execute();
 	$check_car_exists_query->bind_result($car_id);
+	$check_car_exists_query->store_result();
 	$check_car_exists_query->fetch();
-		
+	
 	$insert_user_car_query = $mysqli->prepare('INSERT INTO users_cars (car_nickname, mileage, make, model, year, vin, car_id, engine_type, color, user_id) 
 											   VALUES (?,?,?,?,?,?,?,?,?,?)');
 	$insert_user_car_query->bind_param('sissisissi', $carName, $mileage, $carMake, $carModel, $carYear, $vin, $car_id, $engineType, $carColor, $_SESSION['user_id']);
